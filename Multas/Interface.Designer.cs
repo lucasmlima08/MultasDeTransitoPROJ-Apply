0e064966eb318d04016f3370bef320e3856e22d2
Lucas Myllenno Silva Monteiro Lima <lucas.mlima@hotmail.com.br>
2015-05-17 18:39:13 -0300

    Sistema de Multas.

---
+++ Multas/Interface.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Multas
+{
+    partial class aplicacao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(aplicacao));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.placaEnt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.velocidadeEnt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listaRadares = new System.Windows.Forms.ComboBox();
+            this.botaoMultar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(787, 227);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // placaEnt
+            // 
+            this.placaEnt.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.placaEnt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.placaEnt.Location = new System.Drawing.Point(233, 318);
+            this.placaEnt.Name = "placaEnt";
+            this.placaEnt.Size = new System.Drawing.Size(100, 26);
+            this.placaEnt.TabIndex = 1;
+            this.placaEnt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Futura Bk BT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(760, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Informações do Veículo";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Futura Bk BT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 385);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(760, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Informações do Radar";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // velocidadeEnt
+            // 
+            this.velocidadeEnt.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.velocidadeEnt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.velocidadeEnt.Location = new System.Drawing.Point(507, 318);
+            this.velocidadeEnt.Name = "velocidadeEnt";
+            this.velocidadeEnt.Size = new System.Drawing.Size(100, 26);
+            this.velocidadeEnt.TabIndex = 4;
+            this.velocidadeEnt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label3.Location = new System.Drawing.Point(143, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Placa:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label4.Location = new System.Drawing.Point(357, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 26);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Velocidade:";
+            // 
+            // listaRadares
+            // 
+            this.listaRadares.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.listaRadares.FormattingEnabled = true;
+            this.listaRadares.Location = new System.Drawing.Point(156, 425);
+            this.listaRadares.Name = "listaRadares";
+            this.listaRadares.Size = new System.Drawing.Size(484, 24);
+            this.listaRadares.TabIndex = 7;
+            // 
+            // botaoMultar
+            // 
+            this.botaoMultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.botaoMultar.Location = new System.Drawing.Point(301, 465);
+            this.botaoMultar.Name = "botaoMultar";
+            this.botaoMultar.Size = new System.Drawing.Size(172, 42);
+            this.botaoMultar.TabIndex = 8;
+            this.botaoMultar.Text = "Multar Veículo";
+            this.botaoMultar.UseVisualStyleBackColor = true;
+            this.botaoMultar.Click += new System.EventHandler(this.botaoMultar_Click);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label6.Location = new System.Drawing.Point(613, 318);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 26);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Km/h";
+            // 
+            // aplicacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 553);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.botaoMultar);
+            this.Controls.Add(this.listaRadares);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.velocidadeEnt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.placaEnt);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "aplicacao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Controle de Multas";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox placaEnt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox velocidadeEnt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox listaRadares;
+        private System.Windows.Forms.Button botaoMultar;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Multas/Interface.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace Multas
+{
+    public partial class aplicacao : Form
+    {
+
+        MySqlConnection conexao = new MySqlConnection("Server = localhost; Uid = root; Password = root; Database = SistemaDoDetran; Port = 1313");
+        MySqlCommand comando = new MySqlCommand();
+        MySqlDataReader reader;
+
+        private int velocidadeMaximaPermitida;
+        private int cpfProprietario;
+        private int valorDaMulta;
+
+        String[] radares;
+
+        public aplicacao()
+        {
+            InitializeComponent();
+
+            // Realizar Conexão com o Banco de Dados.
+            conexao.Open();
+            comando.Connection = conexao;
+
+            // Chama radares para a escolha na multa.
+            chamarRadares();
+        }
+
+        private String data()
+        {
+            String data = String.Format("{0:dd/MM/yyyy}", DateTime.Now.Date);
+            return data;
+        } // Retorna data atual.
+
+        private String hora()
+        {
+            String hora = DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+            return hora;
+        } // Retorna hora atual.
+        
+        private void chamarRadares()
+        {
+
+            try
+            {
+                comando.CommandText = "select count(*) from radares";
+                int valor = int.Parse(comando.ExecuteScalar().ToString());
+
+                // Entra aqui se encontra algum registro do Radar.
+                if (valor >= 1)
+                {
+                    radares = new String[valor];
+
+                    comando.CommandText = "select * from radares";
+
+                    reader = comando.ExecuteReader();
+
+                    int acm = 0;
+                    while (reader.Read())
+                    {
+                        String id = reader["ID"].ToString();
+                        String localizacao = reader["LOCALIZACAO"].ToString();
+                        String detalhes = reader["DETALHES"].ToString();
+
+                        String modelo = "Radar " + id + " - " + localizacao + " [" + detalhes + "]";
+
+                        radares[acm] = modelo;
+
+                        listaRadares.Items.Add(modelo);
+                        acm++;
+                    }
+
+                    reader.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Não existe proprietário cadastrado!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Não foi possível listar os proprietarios!");
+            }
+        } // Chama a lista de radares.
+
+        private int numRadar()
+        {
+            for (int i = 0; i < radares.Length; i++)
+            {
+                if (radares[i].Equals(listaRadares.Text))
+                {
+                    return i + 1;
+                }
+            }
+            return 0;
+        } // Encontra o número do radar pela escolha no comboBox.
+
+        private Boolean procurarPlaca(String placa)
+        {
+            try
+            {
+                comando.CommandText = "select count(*) from veiculos where PLACA='" + placa + "';";
+                int valor = int.Parse(comando.ExecuteScalar().ToString());
+
+                if (valor > 0)
+                {
+                    lerCpfProprietario(placa);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        } // Procura se veículo existe.
+
+        private void lerCpfProprietario(String placa)
+        {
+            comando.CommandText = "select proprietario from veiculos where placa='" + placa + "';";
+
+            reader = comando.ExecuteReader();
+
+            String cpf = "";
+
+            while (reader.Read())
+            {
+                cpf = reader["PROPRIETARIO"].ToString();
+            }
+
+            reader.Close();
+
+            cpfProprietario = int.Parse(cpf);
+        }
+        
+        private int ultrapassouVelocidade(String velocidade)
+        {
+            try
+            {
+                comando.CommandText = "select velocidade, peso from radares where ID='" + numRadar() + "';";
+
+                reader = comando.ExecuteReader();
+
+                String velocidadeMaxima = "";
+                String pesoMulta = "";
+
+                while (reader.Read())
+                {
+                    velocidadeMaxima = reader["VELOCIDADE"].ToString();
+                    pesoMulta = reader["PESO"].ToString();
+                }
+
+                velocidadeMaximaPermitida = int.Parse(velocidadeMaxima);
+
+                int diferencaVelocidade = int.Parse(velocidade) - velocidadeMaximaPermitida;
+                valorDaMulta = int.Parse(pesoMulta) * diferencaVelocidade;
+
+                reader.Close();
+
+                return (valorDaMulta);
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        } // Retorna a diferença de velocidade.
+
+        private void pontuarCarteira(int velocidadeAtingida, int velocidadeMaximaPermitida, int cpf)
+        {
+            int pontosNaCarteira = 0;
+            double valorDaMulta = this.valorDaMulta;
+
+            /* Regras de pontuação na carteira */
+            if (velocidadeMaximaPermitida * 2 >= velocidadeAtingida)
+            {
+                pontosNaCarteira = 7;
+            }
+            else if (velocidadeAtingida - velocidadeMaximaPermitida >= velocidadeMaximaPermitida / 2)
+            {
+                pontosNaCarteira = 5;
+            }
+            else
+            {
+                pontosNaCarteira = 4;
+            }
+
+            /* Faz a leitura dos pontos e das multas para realizar a soma */
+            comando.CommandText = "select pontos, multas from proprietarios where cpf='" + cpf + "';";
+
+            reader = comando.ExecuteReader();
+
+            String pontos = "";
+            String multas = "";
+
+            while (reader.Read())
+            {
+                pontos = reader["PONTOS"].ToString();
+                multas = reader["MULTAS"].ToString();
+            }
+
+            reader.Close();
+
+            /* Reescreve somando com os valores atuais */
+
+            pontosNaCarteira += int.Parse(pontos);
+            valorDaMulta += double.Parse(multas);
+
+            comando.CommandText = "update proprietarios set pontos='"+pontosNaCarteira+"', multas='"+valorDaMulta+"' where cpf='"+cpf+"';";
+            comando.ExecuteNonQuery();
+        } // Realiza a pontuação na carteira do proprietário a partir das regras.
+
+        private void multarVeiculo()
+        {
+            if (placaEnt.Text.Equals("") || velocidadeEnt.Text.Equals("") || listaRadares.Text.Equals(""))
+            {
+                MessageBox.Show("Preencha todos os dados!");
+            }
+            else if (procurarPlaca(placaEnt.Text) == false)
+            {
+                MessageBox.Show("Veículo não encontrado!");
+            }
+            else
+            {
+                try
+                {
+                    int velocidadeUltrapassada = ultrapassouVelocidade(velocidadeEnt.Text);
+                    if (velocidadeUltrapassada > 0)
+                    {
+                        pontuarCarteira(int.Parse(velocidadeEnt.Text), velocidadeMaximaPermitida, cpfProprietario);
+
+                        comando.CommandText = "insert into multas(data, hora, placa, radar, velocidade, valor) values " +
+                                              "('" + data() + "','" + hora() + "','" + placaEnt.Text + "','" + numRadar() + "','" + velocidadeEnt.Text + "','" + velocidadeUltrapassada + "');";
+                        comando.ExecuteNonQuery();
+                        MessageBox.Show("Veículo multado!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("O veículo está dentro do limite de Velocidade!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Houve algum problema!" + ex.Message);
+                }
+
+            }
+        } // Evento de multa do veículo.
+        
+        private void botaoMultar_Click(object sender, EventArgs e)
+        {
+            multarVeiculo();
+        } // Evento do Botão para multar veículos.
+
+    }
+}
